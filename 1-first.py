a5166418e868f06cf65f0f5f222b0a478f2f75b3
Marcel Riera <mrier011@fiu.edu>
2019-05-23 17:44:45 -0400

    First commit: added basic example of syntax

---
+++ 1-first.py
@@ -0,0 +1,14 @@
+# First python script example
+
+x = 11
+
+if x < 10:
+    print ("x is less than 10")
+else:
+    print ("x is greather or equal than 10")
+    if x % 2 == 0:
+        print ("x is an even number")
+    else:
+        print ("x is an odd number")
+print ("The value of x is " + str(x))
+
